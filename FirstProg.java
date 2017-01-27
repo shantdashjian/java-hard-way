3f6beb78e937f4e620cb792a4b3d5fb7841a73ca
Shaun Dashjian <shaundashjian@yahoo.com>
2017-01-26 19:21:08 -0700

    Exercise 1: An Important Message

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+    System.out.println( "I, Shaun Dashjian, am determined to learn how to code." );
+    System.out.println( "Today's date is Thursday, January 26, 2017." );
+  }
+}
